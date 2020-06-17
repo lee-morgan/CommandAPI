401da74d02ab2399ab741f05de3d0ccbe673acb6
Lee Morgan <6605757+lee-morgan@users.noreply.github.com>
2020-06-17 07:24:28 +0100

    Add User Secrets

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -5,17 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CommandAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace CommandAPI
 {
     public class Startup
     {
+
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new NpgsqlConnectionStringBuilder();
+            builder.ConnectionString = Configuration.GetConnectionString("PostgreSqlConnection");
+            builder.Username = Configuration["UserID"];
+            builder.Password = Configuration["Password"];
+            services.AddDbContext<CommandContext>(options => 
+                options.UseNpgsql(builder.ConnectionString));
             services.AddControllers();
         }
 
@@ -31,7 +48,7 @@ namespace CommandAPI
 
             app.UseEndpoints(endpoints =>
             {
-              endpoints.MapControllers();
+                endpoints.MapControllers();
             });
         }
     }
